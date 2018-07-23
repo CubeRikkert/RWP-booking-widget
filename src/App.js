0986e1359e51ccfc057c0636d560a551661367aa
alper <alperkay@hotmail.com>
2018-07-23 16:31:17 +0200

    initialization

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Provider} from 'react-redux'
-import store from './store.js'
-import Route from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <main>
-          <Route exact path="/" component={AlbumsListContainer} />
-          </main>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </Provider>
+      <div>
+        <p>Let's book a treatment!</p>
+      </div>
     );
   }
 }
+
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
-import { combineReducers } from 'redux'
-import services from './services'
+import { combineReducers } from 'redux';
 
 export default combineReducers({
-  services
+  
 })
