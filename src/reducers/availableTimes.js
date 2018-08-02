13db9169c7fb191c551bd9b91eeb5d775985f7a5
RichardKorrel <richard.korrel1@gmail.com>
2018-08-02 13:43:09 +0200

    fix TimeForm bug

--- src/reducers/availableTimes.js
+++ src/reducers/availableTimes.js
@@ -8,7 +8,7 @@ export default function(state = null, action) {
     case RESET_FORM:
       return null;
     case RESET_TIMES:
-      return null;
+      return [];
     default:
       return state;
   }
